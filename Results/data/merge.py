8a39b5cde0a3515ecf366ff39d52055a7a6fd375
Jossome <jossome.one@gmail.com>
2019-03-04 19:52:13 -0500

    merge splits

---
+++ Results/data/merge.py
@@ -0,0 +1,20 @@
+'''
+Merge train/val/test sequentially into one matrix.
+'''
+
+import pickle
+import os
+import numpy as np
+
+files = os.listdir('train')
+merged = {x: [] for x in files}
+for f in files:
+    for split in ['train/', 'val/', 'test/']:
+        merged[f].append(pickle.load(open(split + f, 'rb')))
+
+# train: (10179) val: (3481) test: (1611)
+# print(merged[f][0].shape, merged[f][1].shape, merged[f][2].shape)
+
+for f in files:
+    pickle.dump(np.concatenate(merged[f]), open('all/' + f, 'wb'), protocol=2)
+
